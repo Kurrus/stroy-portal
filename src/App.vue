dd758bcb72752cd3710b6733b6d2a40360583b85
ruslan_zbs_org <menrusamen19992@gmail.com>
2020-10-04 15:27:33 +0500

    test project

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Toolbar/>
+    <HelloWorld/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Toolbar from './components/Toolbar.vue'
+import HelloWorld from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Toolbar
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss">
+@import "./assets/scss/style.scss";
 </style>
---
+++ src/components/Toolbar.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="toolbar">
+    <div class="_container">
+      <div class="toolbar__location">
+        <a href="/">
+          <img src="../assets/img/icons/icons.svg#loc" class="svg-loc-dims" alt="loc">
+          <span>Волгоградская область</span>
+        </a>
+      </div>
+      <div class="toolbar__list">
+        <ul class="toolbar__lists">
+          <li><a href="#">Тендеры</a></li>
+          <li><a href="#">База подрядчиков</a></li>
+          <li class="toolbar__list-menu" @click="active = !active">
+            <span>Другие сервисы <img src="../assets/img/icons/icons.svg#chevron" alt=""></span>
+            <ul :class="{_active: active}">
+              <li><a href="#">Тендеры</a></li>
+              <li><a href="#">База подрядчиков</a></li>
+              <li><a href="#">Тендеры</a></li>
+              <li><a href="#">База подрядчиков</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Toolbar',
+  data(){
+    return {
+      active: false
+    }
+  }
+}
+</script>
+
